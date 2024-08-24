cee126ea7ef23c7a93c277f596417794fa95db85
Paulo Roberto <paulonill@gmail.com>
2024-08-24 11:09:14 -0300

    Finish CustomerController and create middware

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prisma } from "../prisma";
 
 class CustomerController {
@@ -18,22 +18,84 @@ class CustomerController {
             console.log(error);
             return res.status(500).json({ error: error });
         }
-
     }
 
-    update() {
+    async update(req: Request, res: Response) {
+        try {
+            const id = req.params.id;
+            const { name, email, document } = req.body; 
 
+            const customerUpdated = await prisma.customers.update({
+                where: {id},
+                data: {
+                    name,
+                    email,
+                    document
+                }
+            });
+            return res.status(200).json(customerUpdated);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: error });
+        }
     }
 
-    delete() {
-
+    async delete(req: Request, res: Response) {
+        try {
+            const id = req.params.id;
+            
+            await prisma.customers.delete({
+                where: {id}
+            });
+            return res.status(204).json();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: error});
+        }
     }
 
-    findAll() {
-
+    async findAll(req: Request, res: Response) {
+        try {
+            const customers = await prisma.customers.findMany();
+            return res.status(200).json(customers);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: error });
+        }
     }
 
-    findById() {
-
+    async findById(req: Request, res: Response) {
+        try {
+            const id = req.params.id;
+            const customer = await prisma.customers.findUnique({
+                where: {id}
+            });
+            if(customer == null) {
+                return res.status(404).json({msg: "Not found."})
+            }
+            return res.status(200).json(customer);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: error });
+        }
     }
+
+    async verifyIfExist(req: Request, res: Response, next: NextFunction) {
+        try {
+            const id = req.params.id; 
+            const customer = await prisma.customers.findUnique({
+                where: {id}
+            });
+            if(customer == null) {
+                return res.status(404).json({msg: "Not found."});
+            }
+            return next();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: error});
+        }
+    }
+
 }
+
+export { CustomerController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,114 +1,18 @@
 import { Router } from "express";
-import {PrismaClient} from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { CustomerController } from "./controllers/CustomerController";
 
 const routes = Router();
 
-/*
- Model
-    id String 
-    name String
-    email String
-    document String
-    createdAt Date now()
-    updatedAt Date now()
-*/
+const customerController = new CustomerController();
 
-// GET POST PUT DELETE
+routes.get("/customers", customerController.findAll);
 
-routes.get("/customers", async (req, res) => {
-    try {
-        const customers = await prisma.customers.findMany();
-        return res.json(customers);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({error: error});
-    }
-});
+routes.get("/customers/:id", customerController.findById);
 
-routes.get("/customers/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const costumer = await prisma.customers.findUnique({
-            where: {id}
-        });
-    
-        if (costumer == null) {
-            return res.status(404).json();
-        }
-    
-        return res.json(costumer);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({error: error});
-    }
-});
+routes.post("/customers", customerController.create);
 
-routes.post("/customers", async (req, res) => {
-    try {
-        const { name, email, document } = req.body;
-        const customer = await prisma.customers.create({
-          data: {
-              name,
-              email,
-              document
-          }
-        });
-        return res.status(201).json(customer);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({error: error});
-    }
-});
+routes.delete("/customers/:id", customerController.verifyIfExist, customerController.delete);
 
-routes.delete("/customers/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const customer = await prisma.customers.findUnique({
-            where: {id}
-        });
-      
-        if (customer == null) {
-            return res.status(404).json();
-        }
-        await prisma.customers.delete({
-            where: {id}
-        });
-        return res.status(204).json();
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({error: error});
-    }
-});
-
-routes.put("/customers/:id", async (req, res) =>{
-   try {
-       const id = req.params.id; 
-       const {name, email, document} = req.body;
-    
-       const customer = await prisma.customers.findUnique({
-           where: {id}
-       });
-    
-       if (customer == null) {
-           return res.status(404).json({msg: "Not found."});
-       }
-    
-       const customerUpdated = await prisma.customers.update({
-           where: {id},
-           data: {
-               name,
-               email,
-               document
-           }
-       });
-       return res.json(customerUpdated);
-   } catch (error) {
-        console.log(error);
-        return res.status(500).json({error: error});
-   } 
-    
-});
+routes.put("/customers/:id", customerController.verifyIfExist, customerController.update);
 
 export { routes }
